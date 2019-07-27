3b4e6d9d28a2ac34ee82e69a6a0411f4a56fde19
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-07-27 16:40:06 -0400

    tab change button added

--- challenge-1-tabs/app.js
+++ challenge-1-tabs/app.js
@@ -1,42 +1,54 @@
-const tabs = document.querySelector('.tabs-component__tabs')
-const positiveTab = document.querySelector('.item-positive')
-const negativeTab = document.querySelector('.item-negative')
-const neutralTab = document.querySelector('.item-neutral')
+const tabs = document.querySelector(".tabs-component__tabs");
+const positiveTab = document.querySelector(".item-positive");
+const negativeTab = document.querySelector(".item-negative");
+const neutralTab = document.querySelector(".item-neutral");
 
-const allTabs = [positiveTab, negativeTab, neutralTab]
+const changeTabButton = document.querySelector(".tabs-component__button");
+const tabInput = document.querySelector(".tabs-component__input");
 
-tabs.addEventListener('click', (e) => {
-    switch (e.target.innerHTML) {
-        case 'Positive':
-            showTabContent(positiveTab, e.target)
-            break;
-        case 'Negative':
-            showTabContent(negativeTab, e.target)
-            break;
-        case 'Neutral':
-            showTabContent(neutralTab, e.target)
-            break;
-    }
-})
+const allTabs = [positiveTab, negativeTab, neutralTab];
+
+tabs.addEventListener("click", e => {
+  switch (e.target.innerHTML) {
+    case "Positive":
+      showTabContent(positiveTab, e.target);
+      break;
+    case "Negative":
+      showTabContent(negativeTab, e.target);
+      break;
+    case "Neutral":
+      showTabContent(neutralTab, e.target);
+      break;
+  }
+});
+
+changeTabButton.addEventListener("click", () => {
+  const tabButton = tabs.children[tabInput.value];
+  const tabContent = allTabs[tabInput.value];
+  tabInput.value === ""
+    ? alert("please enter a valid value to change the tab")
+    : showTabContent(tabContent, tabButton);
+});
 
 function showTabContent(tabContent, tabButton) {
-    let tabsCopy = [...allTabs]
-    tabsCopy.forEach(tab => {
-        tab.classList.remove('content-active')
-    })
-    changeCurrentTab(tabButton)
-    tabContent.classList.add('content-active')
+  let tabsCopy = [...allTabs];
+  tabsCopy.forEach(tab => {
+    tab.classList.remove("content-active");
+  });
+  changeCurrentTab(tabButton);
+  tabContent.classList.add("content-active");
 }
 
 function changeCurrentTab(tabButton) {
-    const allTabButtons = tabs.children
-    for (const item of allTabButtons) {
-        item.classList.remove('active')
-    }
-    tabButton.classList.add('active')
+  const allTabButtons = tabs.children;
+  for (const item of allTabButtons) {
+    item.classList.remove("active");
+  }
+  tabButton.classList.add("active");
 }
 
 // function gets invoced on render
 (function start() {
-    allTabs[0].classList.add('content-active')
-})()
+  allTabs[0].classList.add("content-active");
+  tabInput.max = allTabs.length - 1;
+})();
