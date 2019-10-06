0f0530f8a3ad88667df14f89b6d1dee3d6e173d1
Alejandro Sierra <alexsierra1598@gmail.com>
2019-10-06 18:51:00 -0400

    creating elements and adding them to the video list

--- challenge-4/app.js
+++ challenge-4/app.js
@@ -1,5 +1,9 @@
 const videoList = document.querySelector('.list__side-list')
 const videoItem = document.querySelectorAll('.list__item')
+const addVideoBtn = document.querySelector('.add-video')
+
+let videoNum = 5
+
 const classNameArr = [
     'list__image',
     'list__item-contents',
@@ -11,4 +15,20 @@ videoList.addEventListener('click', (e) => {
     if (classNameArr.includes(className)) {
         console.log('clicked on valid emelent')
     }
+})
+
+addVideoBtn.addEventListener('click', () => {
+    // seperate functions for creating each element
+    let video = document.createElement("div")
+    let image = document.createElement("img")
+    let content = document.createElement("div")
+    content.innerHTML = `Video ${videoNum}`
+    content.classList.add("list__item-contents")
+    image.classList.add("list__image")
+    image.src = "https://picsum.photos/150/75?random"
+    video.appendChild(image)
+    video.classList.add("list__item")
+    videoList.appendChild(video)
+    video.appendChild(content)
+    videoNum++
 })
---
+++ challenge-4/instructions.md
@@ -0,0 +1,7 @@
+## Challenge 4
+
+The objective for this challenge is to add logic in the `app.js` file to allow a click on a video to bring up the information for that specific video.
+
+This is a common thing to do as a front end developer. The extra thing to look out for in this challenge is the fact that videos can be added and they can be removed to listening to the click should always be available on any new or existing videos in the list.
+
+The html and css is setup for you to only need to add code to the app.js file
